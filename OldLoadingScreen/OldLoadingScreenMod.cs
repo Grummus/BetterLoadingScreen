ab0c575c918bdfe46f4adc93f4405ddd4df8f330
Grummus <grahamklatt@msn.com>
2021-07-17 18:42:58 -0700

    Made stuff on settings menu work

--- OldLoadingScreen/OldLoadingScreenMod.cs
+++ OldLoadingScreen/OldLoadingScreenMod.cs
@@ -28,131 +28,173 @@ using VRC.Core;
 
 namespace OldLoadingScreen
 {
-    public class OldLoadingScreenMod : MelonMod
-    {
-        // private GameObject partiallyOffline;
+	public class OldLoadingScreenMod : MelonMod
+	{
+		// private GameObject partiallyOffline;
 
-        // private GameObject cavernDry;
-        private GameObject loadScreenPrefab;
-        // private GameObject newCube;
+		// private GameObject cavernDry;
+		private GameObject loadScreenPrefab;
+		// private GameObject newCube;
 
-        private AssetBundle assets;
-
-
-        public override void OnApplicationStart()
-        {
-            if (typeof(MelonLoader.MelonMod).GetMethod("VRChat_OnUiManagerInit") == null)
-                MelonLoader.MelonCoroutines.Start(GetAssembly());
-        }
-        private System.Collections.IEnumerator GetAssembly()
-        {
-            System.Reflection.Assembly assemblyCSharp = null;
-            while (true)
-            {
-                assemblyCSharp = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(assembly => assembly.GetName().Name == "Assembly-CSharp");
-                if (assemblyCSharp == null)
-                    yield return null;
-                else
-                    break;
-            }
-
-            MelonLoader.MelonCoroutines.Start(WaitForUiManagerInit(assemblyCSharp));
-        }
-        private System.Collections.IEnumerator WaitForUiManagerInit(System.Reflection.Assembly assemblyCSharp)
-        {
-
-            System.Type vrcUiManager = assemblyCSharp.GetType("VRCUiManager");
-            System.Reflection.PropertyInfo uiManagerSingleton = vrcUiManager.GetProperties().First(pi => pi.PropertyType == vrcUiManager);
-
-            while (uiManagerSingleton.GetValue(null) == null)
-                yield return null;
-
-            OnUiManagerInit();
-        }
-
-        // public override void VRChat_OnUiManagerInit()
-        // {
-        //     OnUiManagerInit();
-        // }
-
-        public void OnUiManagerInit()
-        {
-            // while (ReferenceEquals(VRCAudioManager.field_Private_Static_VRCAudioManager_0, null)) yield return null;
-
-            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("OldLoadingScreen.loading.assetbundle"))
-            using (var tempStream = new MemoryStream((int)stream.Length))
-            {
-                stream.CopyTo(tempStream);
-
-                assets = AssetBundle.LoadFromMemory_Internal(tempStream.ToArray(), 0);
-                assets.hideFlags |= HideFlags.DontUnloadUnusedAsset;
-            }
-
-            loadScreenPrefab = assets.LoadAsset_Internal("Assets/Bundle/LoadingBackground.prefab", Il2CppType.Of<GameObject>()).Cast<GameObject>();
-            loadScreenPrefab.hideFlags |= HideFlags.DontUnloadUnusedAsset;
-
-            //cavernDry = assets.LoadAsset_Internal("Assets/Bundle/CavernDry.prefab", Il2CppType.Of<GameObject>()).Cast<GameObject>();
-            //cavernDry.hideFlags |= HideFlags.DontUnloadUnusedAsset;
-
-            //newCube = assets.LoadAsset_Internal("Assets/Bundle/Cube.prefab", Il2CppType.Of<GameObject>()).Cast<GameObject>();
-            //cavernDry.hideFlags |= HideFlags.DontUnloadUnusedAsset;
-
-            OldLoadingScreenSettings.RegisterSettings();
-            CreateGameObjects();
-        }
-
-        public override void OnPreferencesSaved()
-        {
-            // Put your code here you heckin dummy!
-            // Also make a decent method for finding and toggling things within loadScreenPrefab
-
-        }
+		private AssetBundle assets;
 
 
-        private void CreateGameObjects()
-        {
-            MelonLogger.Msg("Finding original GameObjects");
-            var UIRoot = GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup");
-            var InfoPanel = GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/3DElements/LoadingInfoPanel");
-            var SkyCube = GameObject.Find("/UserInterface/MenuContent/Popups/LoadingPopup/3DElements/LoadingBackground_TealGradient/SkyCube_Baked");
-            var bubbles = GameObject.Find("/UserInterface/MenuContent/Popups/LoadingPopup/3DElements/LoadingBackground_TealGradient/_FX_ParticleBubbles");
-            var StartScreen = GameObject.Find("/UserInterface/LoadingBackground_TealGradient_Music/");
-            var originalStartScreenAudio = GameObject.Find("/UserInterface/LoadingBackground_TealGradient_Music/LoadingSound");
-            var originalStartScreenSkyCube = GameObject.Find("/UserInterface/LoadingBackground_TealGradient_Music/SkyCube_Baked");
-            var originalLoadingAudio = GameObject.Find("/UserInterface/MenuContent/Popups/LoadingPopup/LoadingSound");
+		public override void OnApplicationStart()
+		{
+			if (typeof(MelonLoader.MelonMod).GetMethod("VRChat_OnUiManagerInit") == null)
+				MelonLoader.MelonCoroutines.Start(GetAssembly());
+		}
+		private System.Collections.IEnumerator GetAssembly()
+		{
+			System.Reflection.Assembly assemblyCSharp = null;
+			while (true)
+			{
+				assemblyCSharp = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(assembly => assembly.GetName().Name == "Assembly-CSharp");
+				if (assemblyCSharp == null)
+					yield return null;
+				else
+					break;
+			}
 
-            MelonLogger.Msg("Creating new GameObjects");
-            if (OldLoadingScreenSettings.WarpTunnel.Value)
-            {
-                // loadScreenPrefab = CreateGameObject(loadScreenPrefab, new Vector3(400, 400, 400), "UserInterface/MenuContent/Popups/", "LoadingPopup");
-                loadScreenPrefab = CreateGameObject(loadScreenPrefab, new Vector3(400, 400, 400), "UserInterface/MenuContent/Popups/", "LoadingPopup");
-            }
-            // cavernDry = CreateGameObject(cavernDry, new Vector3(400, 400, 400), "UserInterface/", "LoadingBackground_TealGradient_Music");
-            // newCube = CreateGameObject(newCube, new Vector3(0.5f, 0.5f, 0.5f), "UserInterface/", "LoadingBackground_TealGradient_Music");
+			MelonLoader.MelonCoroutines.Start(WaitForUiManagerInit(assemblyCSharp));
+		}
+		private System.Collections.IEnumerator WaitForUiManagerInit(System.Reflection.Assembly assemblyCSharp)
+		{
 
-            MelonLogger.Msg("Disabling original GameObjects");
+			System.Type vrcUiManager = assemblyCSharp.GetType("VRCUiManager");
+			System.Reflection.PropertyInfo uiManagerSingleton = vrcUiManager.GetProperties().First(pi => pi.PropertyType == vrcUiManager);
 
-            if (!OldLoadingScreenSettings.ShowLoadingMessages.Value)
-            {
-                InfoPanel.active = false;
-            }
-            SkyCube.active = false;
-            bubbles.active = false;
-            originalLoadingAudio.active = false;
-            originalStartScreenAudio.active = false;
-            originalStartScreenSkyCube.active = false;
+			while (uiManagerSingleton.GetValue(null) == null)
+				yield return null;
 
-        }
+			OnUiManagerInit();
+		}
 
-        private GameObject CreateGameObject(GameObject obj, Vector3 scale, String rootDest, String parent)
-        {
-            MelonLogger.Msg("Creating " + obj.name);
-            var UIRoot = GameObject.Find(rootDest);
-            var requestedParent = UIRoot.transform.Find(parent);
-            var newObject = Object.Instantiate(obj, requestedParent, false).Cast<GameObject>();
-            newObject.transform.parent = requestedParent;
-            newObject.transform.localScale = scale;
-            return newObject;
-        }
-    }
+		// public override void VRChat_OnUiManagerInit()
+		// {
+		//     OnUiManagerInit();
+		// }
+
+		public void OnUiManagerInit()
+		{
+			// while (ReferenceEquals(VRCAudioManager.field_Private_Static_VRCAudioManager_0, null)) yield return null;
+
+			using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("OldLoadingScreen.loading.assetbundle"))
+			using (var tempStream = new MemoryStream((int)stream.Length))
+			{
+				stream.CopyTo(tempStream);
+
+				assets = AssetBundle.LoadFromMemory_Internal(tempStream.ToArray(), 0);
+				assets.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+			}
+
+			loadScreenPrefab = assets.LoadAsset_Internal("Assets/Bundle/LoadingBackground.prefab", Il2CppType.Of<GameObject>()).Cast<GameObject>();
+			loadScreenPrefab.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+
+			//cavernDry = assets.LoadAsset_Internal("Assets/Bundle/CavernDry.prefab", Il2CppType.Of<GameObject>()).Cast<GameObject>();
+			//cavernDry.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+
+			//newCube = assets.LoadAsset_Internal("Assets/Bundle/Cube.prefab", Il2CppType.Of<GameObject>()).Cast<GameObject>();
+			//cavernDry.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+
+			OldLoadingScreenSettings.RegisterSettings();
+			CreateGameObjects();
+		}
+
+		public override void OnPreferencesSaved()
+		{
+			MelonLogger.Msg("Applying Preferences");
+
+			loadScreenPrefab = GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/LoadingBackground(Clone)");
+			var music = loadScreenPrefab.transform.Find("MenuMusic");
+			var spaceSound = loadScreenPrefab.transform.Find("SpaceSound");
+			var cube = loadScreenPrefab.transform.Find("SkyCube");
+			var particles = loadScreenPrefab.transform.Find("Stars");
+			var warpTunnel = loadScreenPrefab.transform.Find("Tunnel");
+			var logo = loadScreenPrefab.transform.Find("VRCLogo");
+			var InfoPanel = GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/3DElements/LoadingInfoPanel");
+
+
+			if (OldLoadingScreenSettings.ModSounds.Value)
+			{
+				music.gameObject.SetActive(true);
+				spaceSound.gameObject.SetActive(true);
+			} else
+			{
+				music.gameObject.SetActive(false);
+				spaceSound.gameObject.SetActive(false);
+			}
+
+			if (OldLoadingScreenSettings.WarpTunnel.Value)
+			{
+				warpTunnel.gameObject.SetActive(true);
+			} else
+			{
+				warpTunnel.gameObject.SetActive(false);
+			}
+
+			if (OldLoadingScreenSettings.VrcLogo.Value)
+			{
+				logo.gameObject.SetActive(true);
+			} else
+			{
+				logo.gameObject.SetActive(false);
+			}
+
+			if (OldLoadingScreenSettings.ShowLoadingMessages.Value)
+			{
+				InfoPanel.SetActive(true);
+			} else
+			{
+				InfoPanel.SetActive(false);
+			}
+
+		}
+
+
+		private void CreateGameObjects()
+		{
+			MelonLogger.Msg("Finding original GameObjects");
+			var UIRoot = GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup");
+			var InfoPanel = GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/3DElements/LoadingInfoPanel");
+			var SkyCube = GameObject.Find("/UserInterface/MenuContent/Popups/LoadingPopup/3DElements/LoadingBackground_TealGradient/SkyCube_Baked");
+			var bubbles = GameObject.Find("/UserInterface/MenuContent/Popups/LoadingPopup/3DElements/LoadingBackground_TealGradient/_FX_ParticleBubbles");
+			var StartScreen = GameObject.Find("/UserInterface/LoadingBackground_TealGradient_Music/");
+			var originalStartScreenAudio = GameObject.Find("/UserInterface/LoadingBackground_TealGradient_Music/LoadingSound");
+			var originalStartScreenSkyCube = GameObject.Find("/UserInterface/LoadingBackground_TealGradient_Music/SkyCube_Baked");
+			var originalLoadingAudio = GameObject.Find("/UserInterface/MenuContent/Popups/LoadingPopup/LoadingSound");
+
+			MelonLogger.Msg("Creating new GameObjects");
+			loadScreenPrefab = CreateGameObject(loadScreenPrefab, new Vector3(400, 400, 400), "UserInterface/MenuContent/Popups/", "LoadingPopup");
+			// cavernDry = CreateGameObject(cavernDry, new Vector3(400, 400, 400), "UserInterface/", "LoadingBackground_TealGradient_Music");
+			// newCube = CreateGameObject(newCube, new Vector3(0.5f, 0.5f, 0.5f), "UserInterface/", "LoadingBackground_TealGradient_Music");
+
+			MelonLogger.Msg("Disabling original GameObjects");
+
+			// if (!OldLoadingScreenSettings.ShowLoadingMessages.Value)
+			// {
+			//	InfoPanel.active = false;
+			// }
+			SkyCube.active = false;
+			bubbles.active = false;
+			originalLoadingAudio.active = false;
+			originalStartScreenAudio.active = false;
+			originalStartScreenSkyCube.active = false;
+
+
+			OnPreferencesSaved();
+
+		}
+
+		private GameObject CreateGameObject(GameObject obj, Vector3 scale, String rootDest, String parent)
+		{
+			MelonLogger.Msg("Creating " + obj.name);
+			var UIRoot = GameObject.Find(rootDest);
+			var requestedParent = UIRoot.transform.Find(parent);
+			var newObject = Object.Instantiate(obj, requestedParent, false).Cast<GameObject>();
+			newObject.transform.parent = requestedParent;
+			newObject.transform.localScale = scale;
+			return newObject;
+		}
+	}
 }
